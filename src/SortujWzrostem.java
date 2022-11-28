e6a179f25576e15922dac7f42addcd8b951dde30
Maciej Lipski <maciej.lipski2.stud@pw.edu.pl>
2022-11-28 18:55:16 +0100

    Poprawa tempa bubblesorta

--- src/SortujWzrostem.java
+++ src/SortujWzrostem.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 public class SortujWzrostem {
     public static String[] sortujWzrostem(int[] wzrost, String[] imiona){
         for (int i = 0; i < imiona.length ; i++) {
-            for (int j = 0; j < imiona.length-1; j++) {
-                if(wzrost[j]<wzrost[j+1]){
-                    int tempWzrost=wzrost[j+1];
-                    String tempImie=imiona[j+1];
-                    wzrost[j+1]=wzrost[j];
-                    imiona[j+1]=imiona[j];
+            for (int j = 1; j < imiona.length-i; j++) {
+                if(wzrost[j-1]<wzrost[j]){
+                    int tempWzrost=wzrost[j-1];
+                    String tempImie=imiona[j-1];
+                    wzrost[j-1]=wzrost[j];
+                    imiona[j-1]=imiona[j];
                     imiona[j]=tempImie;
                     wzrost[j]=tempWzrost;
                 }
@@ -18,8 +18,8 @@ public class SortujWzrostem {
     }
 
     public static void main(String[] args) {
-        String [] names = {"Andrzej","Maciek","Maciek","Rzepson"};
-        int [] heights = {183,185,179,195};
+        String [] names = {"Andrzej","Maciek","Maciek","Rzepson","Tobiasz","Jędrzej","Kamil"};
+        int [] heights = {183,185,179,195,180,2000,100};
         String[] posortowaneWzrostem= sortujWzrostem(heights,names);
         System.out.println(Arrays.toString(posortowaneWzrostem));
     }
